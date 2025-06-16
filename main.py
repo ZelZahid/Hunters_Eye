790abdcc75e915bca1625d505b157603431933e6
Zelgehai Zahid <zelgehai@gmail.com>
2025-06-16 04:01:43 -0700

    temporary match template added
    +Added temporary needle_image
    +Added code to detect_objects()
        Now finds the needle_image
        returns list of rectangle locations where it finds the needle_image
        detection_queue has tuple of screenshot, and rectangles
    +Draw_and_Output() now draws rectangles where the locations are.
    -FPS dropped down to 22.37 from 45

--- main.py
+++ main.py
@@ -17,11 +17,14 @@ from queue import Queue
 #Globals
 w, h = pyautogui.size() #Captures Screen Resolution [1920x1080]
 monitor_area = {"top":0, "left":0, "width": w, "height": h}
+needle_image = cv.imread("image1.png")
+needle_image = cv.resize(needle_image, (0,0) , fx=0.5, fy=0.5)
+needle_w = needle_image.shape[1]
+needle_h = needle_image.shape[0]
 
 #Queues for thread-safe comms
 screenshot_queue = Queue(maxsize = 3)
 detection_queue = Queue(maxsize = 3)
-
 
 #Thread 1
 def get_screenshot():
@@ -29,18 +32,44 @@ def get_screenshot():
         while True:
             img = sct.grab(monitor_area)
             screenshot = np.array(img)
+            if screenshot.shape[2] == 4:
+                screenshot = cv.cvtColor(screenshot, cv.COLOR_BGRA2BGR) #changes from BGRA to BGR [ stripping alpha val]
+            screenshot = cv.resize(screenshot, (0,0) ,fx=0.5, fy=0.5) #resizing image for better performance 
+
             #if detection is falling behind, dropping old frams lets pipeline tay real-time and not delayed
             if screenshot_queue.full():
                 screenshot_queue.get_nowait() #remove and return an item without waiting, drops oldest screenshot
             screenshot_queue.put(screenshot) #thread-safe put
+
 #Thread 2
 def detect_objects():
     print("Detecting Objects...")
     while True:
         screenshot = screenshot_queue.get()
-        #detection code
+        #detection code-----------
+
+        result = cv.matchTemplate(screenshot, needle_image, method=cv.TM_CCOEFF_NORMED) #returns confidence score
+        threshold = 0.60
+        max_results = 10 #Limiting results #
+        locations = np.where(result >= threshold) #array([334,335]), array [91,92]
+        #zip into position tuples:
+        locations = list(zip(*locations[::-1]))
+
+        #building list
+        rectangles = []
+        for loc in locations:
+            rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+            rectangles.append(rect)
+        #grouping all rectangles that are close to eachother
+        rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
+        #print(rectangles)
+        if len(rectangles) > max_results:
+            print("Too Many Results, raise the threshhold [max_results]")
+            rectangles = rectangles[max_results:] #trancades results if > threshhold
+
+        #--------------------------
         detected_SS = screenshot.copy()
-        detection_queue.put(detected_SS)
+        detection_queue.put((detected_SS,rectangles)) #puts screenshot and rectangle locations in Queue
 
 #Thread 3
 def draw_and_output():
@@ -48,9 +77,10 @@ def draw_and_output():
     n_frames = 1
 
     while True:
-        screenshot = detection_queue.get()
-        output_img = cv.resize(screenshot, (0,0), fx=0.5, fy=0.5) #can move to right after screenshot takes to improve fps
-        cv.imshow("Hunters Eye", output_img)
+        screenshot, rectangles = detection_queue.get()
+        for (x,y,w,h) in rectangles:
+            cv.rectangle(screenshot, (x,y), (x+w,y+h), (0,255,0), 2)
+        cv.imshow("Hunters Eye", screenshot)
 
         if cv.waitKey(1) == ord('q'):
             break
